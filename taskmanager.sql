9b7f7c54b69ee3bbc60dc4e7acf70173319cc524
midhun1356 <144779901+midhun1356@users.noreply.github.com>
2026-06-15 13:36:29 +0530

    Create taskmanager.sql

---
+++ taskmanager.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE taskmanager;
+
+USE taskmanager;
+
+CREATE TABLE users(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+name VARCHAR(100),
+
+email VARCHAR(100),
+
+password VARCHAR(100)
+
+);
+
+CREATE TABLE tasks(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+title VARCHAR(255),
+
+status VARCHAR(50) DEFAULT 'Pending'
+
+);
